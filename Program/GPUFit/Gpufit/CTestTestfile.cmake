2ddefa7fb0cd0052b57f74ad9e7bd210ff4ef23d
Rion Morishita <rionmorishita097@gmail.com>
2025-01-17 15:41:27 +0900

    Deleted un-necessary files.

--- Program/GPUFit/Gpufit/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Dropbox/Jupyter/PostProcessing_BSA/VLIV_Trial/Gpufit-1.2.0
-# Build directory: F:/Dropbox/Jupyter/PostProcessing_BSA/VLIV_Trial/Gpufit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Cpufit")
-subdirs("Gpufit")
-subdirs("examples")
